e326b2a4dbbaa5618675ee9265c6f8e09edd1445
Wajih Uz Zaman <wajihzaman99@gmail.com>
2026-05-31 11:51:19 +0400

    Update main.py

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ async def chatbot(ws: WebSocket):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=7860, reload=True)
